15c950b9d7bdf27b8ca2eaae5909e79f51196363
Natalia Davtyan <natalia.davtyan@gmail.com>
2023-12-12 15:09:39 +0100

    add empty testimonial components

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import Badges from "./components/Badges/Badges"
 import Banners from "./components/Banners/Banners"
 import Cards from "./components/Cards/Cards"
+import Testimonials from "./components/Testimonials/Testimonials"
 
 export default function App() {
 
   return (
       <>
         <h1>Component Library</h1>
-          {/* <Badges />
-          <Banners /> */}
+          <Badges />
+          <Banners />
           <Cards />
+          <Testimonials />
       </>
   )
 }
---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Testimonial from "./Testimonial"
+
+export default function Testimonials() {
+    return (
+        <main>
+            <h2>Testimonials</h2>
+            <div>
+                <Testimonial />
+                <Testimonial />
+                <Testimonial />
+                <Testimonial />
+            </div>
+        </main>
+    )
+}
